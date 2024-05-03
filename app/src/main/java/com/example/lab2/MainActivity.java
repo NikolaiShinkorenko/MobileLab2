bc9005cd89c343ead57f0af34664b9acea11ab20
NikolaiShinkorenko <160001185+NikolaiShinkorenko@users.noreply.github.com>
2024-05-03 16:36:22 +0600

    Added text transfer from EditText to TextView using callbacks

--- app/src/main/java/com/example/lab2/MainActivity.java
+++ app/src/main/java/com/example/lab2/MainActivity.java
@@ -8,11 +8,26 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements OnTextEnteredListener {
+    private EditViewFragment editViewFragment;
+    private TextViewFragment textViewFragment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editViewFragment = new EditViewFragment();
+        textViewFragment = new TextViewFragment();
+
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragmentContainerView2, editViewFragment)
+                .replace(R.id.fragmentContainerView, textViewFragment)
+                .commit();
+
+        editViewFragment.setOnTextEnteredListener(this);
+    }
+
+    @Override
+    public void onTextEntered(String text) {
+        textViewFragment.displayText(text);
     }
 }
